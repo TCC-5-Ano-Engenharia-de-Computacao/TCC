08645069caa35ff1b43df3b6ba9f6e29a8634ca8
su315123 <103009301+su315123@users.noreply.github.com>
2025-04-06 18:26:05 -0300

    feat(InputTest): add input event handling for move, jump, punch, and kick actions
    - Added UnityEvent<string> inputPerformedEvent to handle input events.
    - Implemented event invocation in HandleMovePerformed, HandleJumpPerformed, HandlePunchPerformed, and HandleKickPerformed methods.

--- GangStrike/Assets/Scripts/InputTest.cs
+++ GangStrike/Assets/Scripts/InputTest.cs
@@ -1,13 +1,14 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class InputTest : MonoBehaviour
 {
     private PlayerInput _playerInput;
     private PlayerInputActions _playerInputActions;
-
-
+    public UnityEvent<string> inputPerformedEvent;
     private void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
@@ -34,7 +35,7 @@ public class InputTest : MonoBehaviour
         _playerInputActions.Default.Jump.performed -= HandleJumpPerformed;
         _playerInputActions.Default.Jump.canceled -= HandleJumpCanceled;
         _playerInputActions.Default.Punch.performed -= HandlePunchPerformed;
-        _playerInputActions.Default.Punch.canceled -= HandlePunchCanceled;
+        _playerInputActions.Default.Punch.canceled -= HandlePunchCanceled;  
         _playerInputActions.Default.Kick.performed -= HandleKickPerformed;
         _playerInputActions.Default.Kick.canceled -= HandleKickCanceled;
     }
@@ -45,10 +46,12 @@ public class InputTest : MonoBehaviour
         if (direction > 0)
         {
             Debug.Log($"[D] - Move right ({direction})");
+            inputPerformedEvent.Invoke("Move_Right");
         }
         else
         {
             Debug.Log($"[A] - Move left ({direction})");
+            inputPerformedEvent.Invoke("Move_Left");
         }
     }
     
@@ -60,6 +63,7 @@ public class InputTest : MonoBehaviour
     private void HandleJumpPerformed(InputAction.CallbackContext ctx)
     {
         Debug.Log("[W] - Jump");
+        inputPerformedEvent.Invoke("Jump");
     }
     
     private void HandleJumpCanceled(InputAction.CallbackContext ctx)
@@ -70,6 +74,7 @@ public class InputTest : MonoBehaviour
     private void HandlePunchPerformed(InputAction.CallbackContext ctx)
     {
         Debug.Log("[C] - Punch");
+        inputPerformedEvent.Invoke("Punch");
     }
     
     private void HandlePunchCanceled(InputAction.CallbackContext ctx)
@@ -80,6 +85,7 @@ public class InputTest : MonoBehaviour
     private void HandleKickPerformed(InputAction.CallbackContext ctx)
     {
         Debug.Log("[V] - Kick");
+        inputPerformedEvent.Invoke("Kick");
     }
     
     private void HandleKickCanceled(InputAction.CallbackContext ctx)
