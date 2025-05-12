5d1952cf9fc1684f60c00142b15796df6e507967
edu510 <www.eduardo.f.n@gmail.com>
2025-05-11 22:17:33 -0300

    feat: Creates first addressables

---
+++ GangStrike/Assets/Scripts/AddressableTest.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class AddressableTest : MonoBehaviour
+{
+    public string str;
+    public AssetReferenceT<AudioClip> audio1Reference;
+    public AssetReference testReference;
+}
