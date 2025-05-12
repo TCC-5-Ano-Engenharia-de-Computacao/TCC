3d315ce7ce43c18300eb0ae1d17004c16097543d
edu510 <www.eduardo.f.n@gmail.com>
2025-05-12 00:39:09 -0300

    chore: Removes AddressableTest script

--- GangStrike/Assets/Scripts/AddressableTest.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.AddressableAssets;
-
-public class AddressableTest : MonoBehaviour
-{
-    public string str;
-    public AssetReferenceT<AudioClip> audio1Reference;
-    public AssetReference testReference;
-}
