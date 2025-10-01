31472971e57b5cdd0f9913e59c07e154b756c850
edu510 <www.eduardo.f.n@gmail.com>
2025-09-30 23:59:18 -0300

    Adds Success and Fail Transition Actions

--- GangStrike/Assets/Scripts/Player/NewStateMachine/TransitionMono.cs
+++ GangStrike/Assets/Scripts/Player/NewStateMachine/TransitionMono.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using Player.NewStateMachine.Actions;
 using Player.NewStateMachine.Conditions;
 using StateMachine;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Player.NewStateMachine.Runtime
 {
@@ -13,11 +15,23 @@ namespace Player.NewStateMachine.Runtime
     {
         [SerializeField] private string toId;
         [SerializeField] private List<ConditionBase> conditions = new();
-
+        [SerializeField] private List<ActionBase> successActions = new();
+        [SerializeField] private List<ActionBase> failActions = new();
+        
         public string ToId => toId;
 
         public bool IsValid() => conditions.All(c => c.Evaluate());
 
+        public void OnSuccess()
+        {
+            foreach (var a in successActions) a.Execute();
+        }
+        
+        public void OnFail()
+        {
+            foreach (var a in failActions) a.Execute();
+        }
+        
         public static async Task<TransitionMono> ConstructFromXmlAsync(XElement node, Transform parent, PlayerRoot player)
         {
             var go = new GameObject($"Transition_to_{(string)node.Attribute("to")}");
@@ -26,6 +40,7 @@ namespace Player.NewStateMachine.Runtime
             t.toId = (string)node.Attribute("to");
 
             var condsNode = node.Element("Conditions");
+            
             if (condsNode != null)
             {
                 var created = await Task.WhenAll(
@@ -33,6 +48,27 @@ namespace Player.NewStateMachine.Runtime
                 );
                 foreach (var c in created) if (c != null) t.conditions.Add(c);
             }
+            
+            var successActions = node.Element("SuccessActions");
+            
+            if (successActions != null)
+            {
+                var created = await Task.WhenAll(
+                    successActions.Elements().Select(n => ActionFactory.CreateAsync(n, go.transform, player))
+                );
+                foreach (var a in created) if (a != null) t.successActions.Add(a);
+            }
+            
+            var failActions = node.Element("FailActions");
+            
+            if (failActions != null)
+            {
+                var created = await Task.WhenAll(
+                    failActions.Elements().Select(n => ActionFactory.CreateAsync(n, go.transform, player))
+                );
+                foreach (var a in created) if (a != null) t.failActions.Add(a);
+            }
+            
             return t;
         }
     }
