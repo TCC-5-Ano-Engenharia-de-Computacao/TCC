1130f372dbf358d4febe0275eea72a2a1bb9fe10
edu510 <www.eduardo.f.n@gmail.com>
2025-10-10 20:47:55 -0300

    Adds EnergyBeam exploding animation

--- GangStrike/Assets/Scripts/EnergyBeam.cs
+++ GangStrike/Assets/Scripts/EnergyBeam.cs
@@ -7,6 +7,11 @@ public class EnergyBeam : MonoBehaviour
     [SerializeField] private float speed = 4f;
     [SerializeField] private CharacterRoot attackerCharacterRoot;
     [SerializeField] private IncomingHitBuffer.Hit hit;
+    [SerializeField] private Animator anim;
+    [SerializeField] private float lifetime = 5f;
+    
+    private bool flagStopMoving;
+    private float lifeTimer;
 
     public void InitializeEnergyBeam(CharacterRoot pAttackerCharacterRoot, IncomingHitBuffer.Hit pHit)
     {
@@ -16,7 +21,15 @@ public class EnergyBeam : MonoBehaviour
     
     private void Update()
     {
-        transform.Translate(Vector3.right * (speed * Time.deltaTime));
+        if (!flagStopMoving)
+        {
+            transform.Translate(Vector3.right * (speed * Time.deltaTime));
+        }
+        lifeTimer += Time.deltaTime;
+        if (lifeTimer >= lifetime)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -27,8 +40,14 @@ public class EnergyBeam : MonoBehaviour
             if (enemyCharacterRoot != attackerCharacterRoot)
             {
                 enemyCharacterRoot.incomingHitBuffer.AddHitToBuffer(hit);
-                Destroy(gameObject);
+                anim.SetTrigger("Explode");
+                flagStopMoving = true;
             }
         }
+    }
+
+    private void DestroyEnergyBeam() // Chamado no fim da animação exploding (por Animation Event)
+    {
+        Destroy(gameObject);
     }
 }
